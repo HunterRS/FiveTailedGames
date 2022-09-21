a3c31d775902e9f1a0a834f3a24c3cf442c76fe6
HunterRS <hunterrs01@gmail.com>
2022-09-21 14:03:50 -0400

    Simple Randomization and Deck in order

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -8,8 +8,10 @@ public class CardManager : MonoBehaviour
 
     public Card Card;                   // Card Variable to 
     public static Card[] cardArray;     // Array of all cards
+    public Card[] cardArrayTemp;     // Array of all cards
     public int CardNum;                 // Number of cards (Increase when we add more)
     private Card placeholder;
+    private Card NewCard;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -19,9 +21,18 @@ public class CardManager : MonoBehaviour
     void Start()
     {
         cardArray = new Card[CardNum]; // creates Arrays with number of entries equal to cardNum
-        placeholder = (Card)Instantiate(Card, new Vector2(1.0f, 1.0f), Quaternion.identity);    // creates card, attaching it to placeholder.
-        cardArray[0] = placeholder;         // pushes placeholder into array
-        Card.Destroy(placeholder);           // deletes placeholder, so card data is still stored in cardArray
+        for (int i = 0; i < CardNum; i++)
+        {
+            Debug.Log(cardArray[i]);
+            Debug.Log(cardArrayTemp[i]);
+            cardArray[i] = cardArrayTemp[i];
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            NewCard = cardArray[Random.Range(0, cardArray.Length)];
+            Instantiate(NewCard, new Vector2(i* 40-40, 0), Quaternion.identity);    // creates card, attaching it to placeholder.
+        }
     }
 
     // Update is called once per frame
