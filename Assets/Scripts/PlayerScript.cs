7e8ce546d4883e0bc90e9c34ba43072cff73c052
Xeaox90 <ngreiner90@gmail.com>
2022-10-26 21:10:39 -0400

    class update final

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour
     public float charSpeed;
     public float maxSpeed;
     private Rigidbody RB;
+    private bool running;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,21 +20,25 @@ public class PlayerScript : MonoBehaviour
         if(RB.velocity.magnitude <= maxSpeed){
             if(Input.GetKey("a")){
             RB.velocity += gameObject.transform.forward * charSpeed;
+            running = true;
         }
             if(Input.GetKey("d")){
             RB.velocity += gameObject.transform.forward * -charSpeed;
+            running = true;
         }
         if(Input.GetKey("w")){
             RB.velocity += gameObject.transform.right * charSpeed;
+            running = true;
         }
             if(Input.GetKey("s")){
             RB.velocity += gameObject.transform.right * -charSpeed;
+            running = true;
         }
 
         
         }
         if(Input.GetKeyDown("space")){
-            RB.AddForce(gameObject.transform.up * charSpeed * 10000);
+            RB.AddForce(gameObject.transform.up * charSpeed * 2000);
         }      
 
 
