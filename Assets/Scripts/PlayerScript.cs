1202d25bd07014ee985f539a5533a44b76e9cdb6
Xeaox90 <77757197+Xeaox90@users.noreply.github.com>
2022-10-28 00:18:27 -0400

    Oct 28 12:17am
    osin moves with animation
    moper scales correctly
    added potential corruption amulet

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,13 +6,18 @@ public class PlayerScript : MonoBehaviour
 {
     public float charSpeed;
     public float maxSpeed;
+    public GameObject playerMeshObj;
+
     private Rigidbody RB;
     private bool running;
-    public Animator playerAnim;
+    private Animator playerAnim;
+
+    private Vector3 runDirection;
     // Start is called before the first frame update
     void Start()
     {
         RB = GetComponent<Rigidbody>();
+        playerAnim = playerMeshObj.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,25 +28,32 @@ public class PlayerScript : MonoBehaviour
             if(Input.GetKey("a")){
             RB.velocity += gameObject.transform.forward * charSpeed;
             running = true;
+            runDirection = new Vector3(RB.velocity.x, 0, RB.velocity.z);
+
         }
-            if(Input.GetKey("d")){
+        if(Input.GetKey("d")){
             RB.velocity += gameObject.transform.forward * -charSpeed;
             running = true;
+            runDirection = new Vector3(RB.velocity.x, 0, RB.velocity.z);
+
         }
         if(Input.GetKey("w")){
             RB.velocity += gameObject.transform.right * charSpeed;
             running = true;
+            runDirection = new Vector3(RB.velocity.x, 0, RB.velocity.z);
+
         }
-            if(Input.GetKey("s")){
+        if(Input.GetKey("s")){
             RB.velocity += gameObject.transform.right * -charSpeed;
             running = true;
+            runDirection = new Vector3(RB.velocity.x, 0, RB.velocity.z);
         }
         if(running)
             playerAnim.SetBool("Run",true);
         else
             playerAnim.SetBool("Run",false);
 
-        
+        playerMeshObj.transform.LookAt(playerMeshObj.transform.position + runDirection);
         }
         if(Input.GetKeyDown("space")){
             RB.AddForce(gameObject.transform.up * charSpeed * 2000);
